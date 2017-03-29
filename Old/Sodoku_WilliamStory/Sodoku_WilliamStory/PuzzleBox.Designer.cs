e1614d7f798698c03dd5e05c3fe1e0eff1f02101
WilliamEStory <william.story.e@gmail.com>
2017-03-29 14:45:18 -0400

    Add files via upload

---
+++ Old/Sodoku_WilliamStory/Sodoku_WilliamStory/PuzzleBox.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Sodoku_WilliamStory
+{
+    partial class PuzzleBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ViewPuzzle = new System.Windows.Forms.DataGridView();
+            this.C1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.b_Solve = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewPuzzle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ViewPuzzle
+            // 
+            this.ViewPuzzle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ViewPuzzle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.C1,
+            this.C2,
+            this.C3,
+            this.C4,
+            this.C5,
+            this.C6,
+            this.C7,
+            this.C8,
+            this.C9});
+            this.ViewPuzzle.Location = new System.Drawing.Point(3, 5);
+            this.ViewPuzzle.Name = "ViewPuzzle";
+            this.ViewPuzzle.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.ViewPuzzle.Size = new System.Drawing.Size(510, 364);
+            this.ViewPuzzle.TabIndex = 0;
+            this.ViewPuzzle.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ViewPuzzle_CellContentClick);
+            // 
+            // C1
+            // 
+            this.C1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C1.HeaderText = "C1";
+            this.C1.Name = "C1";
+            this.C1.ReadOnly = true;
+            this.C1.Width = 45;
+            // 
+            // C2
+            // 
+            this.C2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C2.HeaderText = "C2";
+            this.C2.Name = "C2";
+            this.C2.ReadOnly = true;
+            this.C2.Width = 45;
+            // 
+            // C3
+            // 
+            this.C3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C3.HeaderText = "C3";
+            this.C3.Name = "C3";
+            this.C3.ReadOnly = true;
+            this.C3.Width = 45;
+            // 
+            // C4
+            // 
+            this.C4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C4.HeaderText = "C4";
+            this.C4.Name = "C4";
+            this.C4.ReadOnly = true;
+            this.C4.Width = 45;
+            // 
+            // C5
+            // 
+            this.C5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C5.HeaderText = "C5";
+            this.C5.Name = "C5";
+            this.C5.ReadOnly = true;
+            this.C5.Width = 45;
+            // 
+            // C6
+            // 
+            this.C6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C6.HeaderText = "C6";
+            this.C6.Name = "C6";
+            this.C6.ReadOnly = true;
+            this.C6.Width = 45;
+            // 
+            // C7
+            // 
+            this.C7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C7.HeaderText = "C7";
+            this.C7.Name = "C7";
+            this.C7.ReadOnly = true;
+            this.C7.Width = 45;
+            // 
+            // C8
+            // 
+            this.C8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C8.HeaderText = "C8";
+            this.C8.Name = "C8";
+            this.C8.ReadOnly = true;
+            this.C8.Width = 45;
+            // 
+            // C9
+            // 
+            this.C9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.C9.HeaderText = "C9";
+            this.C9.Name = "C9";
+            this.C9.ReadOnly = true;
+            this.C9.Width = 45;
+            // 
+            // b_Solve
+            // 
+            this.b_Solve.Location = new System.Drawing.Point(44, 407);
+            this.b_Solve.Name = "b_Solve";
+            this.b_Solve.Size = new System.Drawing.Size(75, 23);
+            this.b_Solve.TabIndex = 1;
+            this.b_Solve.Text = "Solve!";
+            this.b_Solve.UseVisualStyleBackColor = true;
+            this.b_Solve.Click += new System.EventHandler(this.b_Solve_Click);
+            // 
+            // PuzzleBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 459);
+            this.Controls.Add(this.b_Solve);
+            this.Controls.Add(this.ViewPuzzle);
+            this.Name = "PuzzleBox";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ViewPuzzle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ViewPuzzle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C9;
+        private System.Windows.Forms.Button b_Solve;
+    }
+}
+
---
+++ Old/Sodoku_WilliamStory/Sodoku_WilliamStory/PuzzleBox.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Sodoku_WilliamStory
+{
+    public partial class PuzzleBox : Form
+    {
+        private int[,] sodokuPuzzle;
+        private List<int> currentNonet;
+        private int placedNumbers = 0;
+
+        private Thread solveThread, popThread;
+
+        public PuzzleBox()
+        {
+            InitializeComponent();
+            loadPuzzle();
+        }
+
+        #region LOADING PUZZLE / UI UPDATING
+        private void loadPuzzle()
+        {
+            sodokuPuzzle = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sodokuPuzzle[i, j] = 0;
+                }
+            }
+
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.Filter = "Text Files (.txt)|*.txt";
+            oFD.FilterIndex = 1;
+            oFD.ShowDialog();
+
+            string currLine = "";
+            string[,] initialPuzzle = new string[9, 9];
+            char delim = ',';
+            int currentRow = 0; //Need to keep track of what row we're on while parsing the puzzle.
+
+            StreamReader puzzleReader = new StreamReader(oFD.FileName);
+            try
+            {
+                while ((currLine = puzzleReader.ReadLine()) != null)
+                {
+                    string[] row = currLine.Split(delim);
+                    for (int i = 0; i < row.Length; i++)
+                    {
+                        if (!(row[i].Equals("")))
+                        {
+                            int number = 0;
+                            Int32.TryParse(row[i], out number);
+                            sodokuPuzzle[currentRow, i] = number;
+                        }
+                    }
+                    currentRow++;
+                }
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Exit();
+            }
+            puzzleReader.Close();
+            populateDGV();
+        }
+        private void populateDGV()
+        {
+            //Set up row headers so it's easy to see the row numbers
+            for (int i = 0; i < 9; i++)
+            {
+                ViewPuzzle.Rows.Add();
+                ViewPuzzle.Rows[i].HeaderCell.Value = "R" + (1+i);
+                ViewPuzzle.Rows[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+
+            //Populate the rows
+            popThread = new Thread(() =>
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        for (int k = 0; k < 9; k++)
+                        {
+                            ViewPuzzle.Rows[i].Cells[k].Value = sodokuPuzzle[i, k];
+                            ViewPuzzle.Invoke((MethodInvoker)delegate
+                            {
+                                if (sodokuPuzzle[i, k] != 0)
+                                {
+                                    ViewPuzzle.Rows[i].Cells[k].Style.BackColor = Color.Aquamarine;
+                                }
+                            });
+                        }
+                    }
+                    popThread.Join();
+                }
+            );
+            popThread.Start();
+        }
+        #endregion
+
+        #region SOLVE PUZZLE
+
+        private void b_Solve_Click(object sender, EventArgs e)
+        {
+            solvePuzzle();
+        }
+
+        
+        private void solvePuzzle()
+        {
+            bool giveUp = false; //Quit if program can't figure out the puzzle.
+            int passes = 0; //Current amount of passes without placing a number.
+            List<int> possibleAnswers; //Possible answers for this current box.
+            solveThread = new Thread(() =>
+                {
+                    while (!giveUp)
+                    {
+                        //Step through each box
+                        for (int i = 0; i < 9; i++)
+                        {
+                            for (int k = 0; k < 9; k++)
+                            {
+                                getPossibleAnswers(k, i, out possibleAnswers);
+                                if (possibleAnswers.Count == 1 && sodokuPuzzle[k, i] == 0)
+                                {
+                                    ViewPuzzle.Invoke((MethodInvoker)delegate
+                                        {
+                                            ViewPuzzle.Rows[k].Cells[i].Value = possibleAnswers[0];
+                                            ViewPuzzle.Rows[k].Cells[i].Style.BackColor = Color.Green;
+                                            sodokuPuzzle[k, i] = possibleAnswers[0];
+                                            placedNumbers++;
+                                        }
+                                    );
+                                    Thread.Sleep(150);
+                                }
+                            }
+                        }
+                        passes++;
+                        if (passes == 30)
+                        {
+                            giveUp = true;
+                            MessageBox.Show("Solved to the best of my ability!" +"\n"
+                                +"Placed numbers: " + placedNumbers);
+                        }
+                            
+                    }
+                    solveThread.Join();
+                }
+            );
+            solveThread.Start();
+        }
+
+        //First method: Look to the left/right/up/down and see if there is an answer.
+        private void getPossibleAnswers(int row, int column, out List<int> possAnswers)
+        {
+            possAnswers = new List<int>();
+            for (int i = 0; i < 9; i++)
+            {
+                possAnswers.Add(i + 1);
+            }
+
+            if (sodokuPuzzle[row, column] != 0)//This box is already filled in, so no need to solve it.
+            {
+                return;
+            }
+
+            //Build the current nonet square we're in.
+            currentNonet = new List<int>();
+            if (column % 3 == 0 && row % 3 == 0)
+            {
+                currentNonet.Clear();
+                //In the upper left corner of the nonet square
+                //Now it's just a matter of looping a 3x3 grid
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        int boxValue = sodokuPuzzle[i + row, k + column];
+                        if (boxValue != 0)
+                        {
+                            currentNonet.Add(boxValue);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                currentNonet.Clear();
+                //Find out where the upper left hand corner of this square is.
+                int startRow = row - (row % 3); //upper corner.
+                int startColumn = column - (column % 3); //most left column.
+
+                //Go through the nonet square
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        int boxValue = sodokuPuzzle[i + startRow, k + startColumn];
+                        if (boxValue != 0)
+                        {
+                            currentNonet.Add(boxValue);
+                        }
+                    }
+                }
+            }
+
+            //Remove nonent from possible answers.
+            for (int i = 0; i < currentNonet.Count; i++)
+            {
+                possAnswers.Remove(currentNonet[i]);
+            }
+
+            //Let's look at the row we're on first
+            for (int i = 0; i < 9; i++)
+            {
+                int boxValue = sodokuPuzzle[row, i];
+                if (boxValue != 0)
+                {
+                    possAnswers.Remove(boxValue);
+                }
+            }
+            //Now lets take a look at what column we're on.
+            for (int i = 0; i < 9; i++)
+            {
+                int boxValue = sodokuPuzzle[i, column];
+                if (boxValue != 0)
+                {
+                    possAnswers.Remove(boxValue);
+                }
+            }
+            
+        }
+
+        #endregion
+
+        private void ViewPuzzle_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
